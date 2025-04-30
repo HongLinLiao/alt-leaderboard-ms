2841194209eda0a7ecba54c6feae513ac4d981fb
deyoliu <icytape620@gmail.com>
2025-04-30 20:02:26 +0800

    add footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Image from 'next/image';
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,40 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* 主要內容 */}
         {children}
+
+        {/* 頁尾 */}
+        <footer className="footer">
+          <div className="footerContent">
+            <div className="footerLeft">
+              <p className="footerText">© Artale-TW Alt Leaderboard by Kap</p>
+            </div>
+            <div className="footerCenter">
+              {/* Discord Icon Link */}
+              <a href="https://discord.com/users/78837614261571584" target="_blank" rel="noopener noreferrer" className="socialIcon">
+                <Image
+                  src="/discord-icon.svg"
+                  alt="Discord"
+                  width={24}
+                  height={24}
+                />
+              </a>
+              {/* GitHub Icon Link */}
+              {/* <a href="你的GitHub倉庫連結" target="_blank" rel="noopener noreferrer" className="socialIcon">
+                <Image
+                  src="/icons/github-icon.svg"
+                  alt="GitHub"
+                  width={24}
+                  height={24}
+                />
+              </a> */}
+            </div>
+            <div className="footerRight">
+              <p className={`footerText footerDisclaimer`}>本站資料由Nexon Korea Corp及Artale提供，僅供參考</p>
+            </div>
+          </div>
+        </footer>
       </body>
     </html>
   );
